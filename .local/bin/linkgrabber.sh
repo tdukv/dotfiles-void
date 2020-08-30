8dd09bb46d0593d9b92bb30e0fb8e69baec86e39
tdu <tdukv@protonmail.com>
2020-08-30 23:05:09 +0300

    fix linkgrabber assuming input is binary

--- .local/bin/linkgrabber.sh
+++ .local/bin/linkgrabber.sh
@@ -4,5 +4,5 @@
 # https://st.suckless.org/patches/externalpipe/linkgrabber.sh
 
 regex='(((http|https|ftp|gopher)|mailto)[.:][^ >"\t]*|www\.[-a-z0-9.]+)[^ .,;\t>">\):]'
-url=$(grep -Po "$regex" | dmenu -p "Go:") || exit
+url=$(grep -aPo "$regex" | dmenu -p "Go:") || exit
 [ -n "$url" ] && $BROWSER "$url"
