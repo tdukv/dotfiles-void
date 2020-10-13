80cea150cb5d607b1d16ebb358024952057edc4f
tdu <tdukv@protonmail.com>
2020-10-13 22:51:15 +0300

    pick a program with linkgrabber

--- .local/bin/linkgrabber.sh
+++ .local/bin/linkgrabber.sh
@@ -5,4 +5,13 @@
 
 regex='(((http|https|ftp|gopher)|mailto)[.:][^ >"\t]*|www\.[-a-z0-9.]+)[^ .,;\t>">\):]'
 url=$(grep -aPo "$regex" | dmenu -p "Go:") || exit
-[ -n "$url" ] && $BROWSER "$url"
+
+case "$0" in
+  *linkgrabber_dmenu)
+      prg=$(printf "%s\nmpv\nfeh\n" "$BROWSER" | dmenu -p "open with") ;;
+  *)
+      prg="$BROWSER" ;;
+esac
+
+[ -z "$prg" ] && exit
+[ -n "$url" ] && $prg "$url"
